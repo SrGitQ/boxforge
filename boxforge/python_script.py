98a25d5fb34b9325c1969dcf8e63916202724f54
JMBalanzar <1909011@upy.edu.mx>
2024-02-18 23:43:12 -0600

    python script default metadata

--- boxforge/python_script.py
+++ boxforge/python_script.py
@@ -4,7 +4,7 @@ from boxforge.util import ElementInterface
 
 class PythonScript(ElementInterface):
     """Ignition python script, python 2.7 -- Jython 2.7"""
-    def __init__(self, name:str, script_path:str, metadata: Metadata) -> None:
+    def __init__(self, name:str, script_path:str, metadata: Metadata = {}) -> None:
         self._name = name
         self._script_path = script_path
         self._metadata = Metadata(metadata)
